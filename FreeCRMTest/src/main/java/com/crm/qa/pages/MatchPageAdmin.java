88bf603d51d78a8ae5659de3a65da2a846859018
no02 <no02@192.168.1.206>
2023-08-01 16:48:19 +0530

    Update script-MatchPageAdmin

--- FreeCRMTest/src/main/java/com/crm/qa/pages/MatchPageAdmin.java
+++ FreeCRMTest/src/main/java/com/crm/qa/pages/MatchPageAdmin.java
@@ -1,7 +1,22 @@
 package com.crm.qa.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import com.crm.qa.base.TestBase;
 
 public class MatchPageAdmin extends TestBase{
+	//Page factory or Object repository
+	@FindBy(id="filter-search-input")
+	WebElement valueFilter;
+	
+	@FindBy(xpath="//*[@id=\"filter\"]/div[1]/div/span")
+	WebElement clickCancel;
+	
+	@FindBy(xpath="//*[@id=\"dropdown-assignment\"]/div/div[1]")
+	WebElement clickAssignmentDropDown;
+	
+	@FindBy(xpath="//*[@id=\"dropdown-assignment\"]/div/div[3]/ul/li[7]/span/span")
+	WebElement selectAssignmentValue;
 
 }
