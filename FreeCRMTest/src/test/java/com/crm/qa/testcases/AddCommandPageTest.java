5310e3c4527bd8200be2dd75820a1a6ec575cc10
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-07-11 17:42:09 +0530

    AddCommandPageTest

--- FreeCRMTest/src/test/java/com/crm/qa/testcases/AddCommandPageTest.java
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/AddCommandPageTest.java
@@ -34,9 +34,9 @@ public class AddCommandPageTest extends TestBase{
 		checkCommandPage=new CheckCommandPage();
 		testUtil=new TestUtil();
 		homePage=loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
-		testUtil.testWaitEight();
-		homePage.clickAcceptAllCookies();
-		testUtil.testWaitEight();
+		testUtil.testWaitFourteen();
+		//homePage.clickAcceptAllCookies();
+		//testUtil.testWaitEight();
 		homePage.clickMyAssignmentsLink();
 		testUtil.testWaitFour();
 		myAssignmentsPage.clickAddNewCommand();
