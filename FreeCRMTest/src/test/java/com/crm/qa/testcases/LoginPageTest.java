cee9230b0f38871d2a7078955d70b4c1f0b7ca69
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-07-11 19:15:59 +0530

    LoginPageTest

--- FreeCRMTest/src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -7,10 +7,12 @@ import org.testng.annotations.Test;
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
 
 public class LoginPageTest extends TestBase{
 	LoginPage loginPage;
 	HomePage homePage;
+	TestUtil testUtil;
 
 	public LoginPageTest(){
 		super();
@@ -20,23 +22,29 @@ public class LoginPageTest extends TestBase{
 	public void setUp() {
 		initialization();
 		loginPage=new LoginPage();
+		testUtil=new TestUtil();
 	}
 	
 	@Test(priority=1)
-	public void loginPageTitleTest() {
+	public void loginPageTitleTest()throws Exception {
 		String title=loginPage.validateLoginPageTitle();
+		testUtil.testWaitFour();
 		Assert.assertEquals(title, "Log in | Younited® platform");
+		testUtil.testWaitFour();
 	}
 	
 	@Test(priority=2)
-	public void imageLogoTest() {
+	public void imageLogoTest()throws Exception {
 		boolean flag=loginPage.validateImageLogo();
+		testUtil.testWaitFour();
 		Assert.assertTrue(flag);
+		testUtil.testWaitFour();
 	}
 	
 	@Test(priority=3)
-	public void loginTest() {
+	public void loginTest()throws Exception {
 		homePage=loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		testUtil.testWaitFourteen();
 	}
 	
 	
