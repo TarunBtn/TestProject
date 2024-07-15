f7f1f543c72534d3892c35e3bb5a622ff516ecdf
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-07-15 19:24:07 +0530

    TestBase

--- FreeCRMTest/src/main/java/com/crm/qa/base/TestBase.java
+++ FreeCRMTest/src/main/java/com/crm/qa/base/TestBase.java
@@ -39,14 +39,14 @@ public class TestBase {
 		if(browserName.equals("chrome")) {
 			System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
 			//WebDriverManager.chromedriver().setup();
-			ChromeOptions options = new ChromeOptions();
+			/*ChromeOptions options = new ChromeOptions();
 			options.addArguments("--headless");
 			options.addArguments("--no-sandbox");
 			options.addArguments("--disable-dev-shm-usage");
 			options.addArguments("--disable-gpu");
 			options.addArguments("--window-size=1920,1080");
-			Driver=new ChromeDriver(options);
-			//Driver=new ChromeDriver();		
+			Driver=new ChromeDriver(options);*/
+			Driver=new ChromeDriver();		
 		}else if(browserName.equals("FF")) {
 			System.setProperty("webdriver.gecko.driver", "C:\\geckodriver\\geckodriver.exe");
 			Driver=new FirefoxDriver();
@@ -56,7 +56,9 @@ public class TestBase {
 		Driver.manage().deleteAllCookies();
 		Driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
 		Driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		Thread.sleep(4000);
 		Driver.get(prop.getProperty("url"));
+		Thread.sleep(4000);
 		
 	}
 
