5d3b5ae9d0fc96b852e0dad0bc7db285a3ac3f63
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-07-26 12:32:17 +0530

    RegisteredWizard

--- FreeCRMTest/src/main/java/com/crm/qa/pages/RegisteredWizard.java
+++ FreeCRMTest/src/main/java/com/crm/qa/pages/RegisteredWizard.java
@@ -1,11 +1,48 @@
 package com.crm.qa.pages;
 
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.support.PageFactory;
+
 import com.crm.qa.base.TestBase;
 
 public class RegisteredWizard extends TestBase{
 	
 	//Pagefactory OR Object Repository
 	
+	
+	//Initialization
+	public RegisteredWizard() {
+		PageFactory.initElements(Driver, this);
+	}
+	
+	//Action
+	public static void setClipboardData(String string) {
+		//StringSelection is a class that can be used for copy and paste operations.
+		   StringSelection stringSelection = new StringSelection(string);
+		   Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+		}
+	
+    public static void uploadFile(String fileLocation) {
+       try {
+        	//Setting clipboard with file location
+            setClipboardData(fileLocation);
+            //native key strokes for CTRL, V and ENTER keys
+            Robot robot = new Robot();
+	
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+       }catch (Exception exp) {
+        	exp.printStackTrace();
+       }
+    }
 	
 
 }
