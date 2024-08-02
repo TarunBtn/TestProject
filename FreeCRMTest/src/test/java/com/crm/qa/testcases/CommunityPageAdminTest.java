863ae9b8701674be41fdc88fd5609c60ca78e045
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-08-02 14:38:38 +0530

    CommunityPageAdminTest

--- FreeCRMTest/src/test/java/com/crm/qa/testcases/CommunityPageAdminTest.java
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/CommunityPageAdminTest.java
@@ -1,5 +1,6 @@
 package com.crm.qa.testcases;
 
+import org.openqa.selenium.ElementNotInteractableException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -52,7 +53,11 @@ public class CommunityPageAdminTest extends TestBase{
 		myNetworkPageFree.clickCancelSearch();
 		testUtil.testWaitEight();
 		//Type of Message
-		myNetworkPageFree.clickTypeMessageDropDown();
+		try {
+		    myNetworkPageFree.clickTypeMessageDropDown();
+		}catch(ElementNotInteractableException e) {
+			e.printStackTrace();
+		}
 		testUtil.testWaitFour();
 		myNetworkPageFree.selectTypeMessage();
 		//testUtil.testWaitFour();
