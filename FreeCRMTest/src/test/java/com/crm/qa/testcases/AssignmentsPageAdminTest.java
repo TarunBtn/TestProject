7421e3248687aba2ce780ffdcdad2780c0641ab0
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-08-02 13:28:14 +0530

    AssignmentsPageAdminTest

--- FreeCRMTest/src/test/java/com/crm/qa/testcases/AssignmentsPageAdminTest.java
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/AssignmentsPageAdminTest.java
@@ -51,7 +51,11 @@ public class AssignmentsPageAdminTest extends TestBase{
 		assignmentsPageFree.clearAssignmentsFilter();
 		testUtil.testWaitEight();
 		//Sector
-		assignmentsPageFree.clickSectorTab();
+		try {
+		    assignmentsPageFree.clickSectorTab();
+		}catch(ElementNotInteractableException e) {
+			e.printStackTrace();
+		}
 		testUtil.testWaitFour();
 		assignmentsPageFree.selectSector();
 		//testUtil.testWaitFour();
